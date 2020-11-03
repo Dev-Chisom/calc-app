6f92875af2de8a9bb83bff18bd480cef561d4fd8
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-11-03 21:23:10 +0100

    my commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// const myName = prompt("Enter your name");
+ 
  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
@@ -14,44 +16,50 @@
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-  const calc = () => {
+
+  let myName =  prompt('Enter your name');
+  const calc = (e) => {
+
+     e.preventDefault();
       //get message
     const num1 = parseFloat(document.getElementById("n1").value)
     const num2 = parseFloat(document.getElementById("n2").value)
     // const result = document.getElementById("result");
     const operate = document.getElementById("opr").value;
 
-
-    const show = document.querySelector('.show');
+     let ans;
+    
 
     if(operate === '+'){
-       let ans = document.getElementById('result').value = num1+num2;
-       show.innerHTML = `${num1} ${operate} ${num2} = ${ans}`;
+        ans = document.getElementById('result').value = num1+num2;
+      //  show.innerHTML = `${myName} : ${num1} ${operate} ${num2} = ${ans}`;
     }
     if(operate === '-'){
-       let ans = document.getElementById('result').value = num1-num2;
-       show.innerHTML = `${num1} ${operate} ${num2} = ${ans}`;
+        ans = document.getElementById('result').value = num1-num2;
+      //  show.innerHTML = `${myName} : ${num1} ${operate} ${num2} = ${ans}`;
     }
     if(operate === '/'){
-        let ans = document.getElementById('result').value = num1/num2;
-        show.innerHTML = `${num1} ${operate} ${num2} = ${ans}`;
+         ans = document.getElementById('result').value = num1/num2;
+      //   show.innerHTML = `${myName} :  ${num1} ${operate} ${num2} = ${ans}`;
     }
     if(operate === 'x'){
-       let ans = document.getElementById('result').value = num1*num2;
-       show.innerHTML = `${num1} ${operate} ${num2} = ${ans}`;
+       ans = document.getElementById('result').value = num1 * num2;
+      //  show.innerHTML = `${myName} : ${num1} ${operate} ${num2} = ${ans}`;
     }
     if(operate === '%'){
-       let ans = document.getElementById('result').value = num1%num2;
-       show.innerHTML = `${num1} ${operate} ${num2} = ${ans}`;
+       ans = document.getElementById('result').value = num1%num2;
+      //  show.innerHTML = `${myName} : ${num1} ${operate} ${num2} = ${ans}`;
     }
 
     //save in database
     firebase.database().ref('results').push().set({
+        "sender":myName,  
         "n1": num1,
         "n2": num2,
         "opr":operate,
-        "show": show,
-        "result": result
+        // "show": show,
+        "result": ans,
+        
        
     })
 
@@ -59,13 +67,23 @@
   }
 
   //Listen for incoming messages
-  firebase.database().ref("results").on("child_added", function(snapshot) {
+  firebase.database().ref("results").limitToLast(10).on("child_added", function(snapshot) {
+   const show = document.querySelector('.show');
+   const sender = snapshot.val().sender;
+   const n1 =snapshot.val().n1;
+   const n2 = snapshot.val().n2;
+   const res =snapshot.val().result;
+   const operate = document.getElementById("opr").value;
+
+   
     let html ="";
     html += "<p>";
-    html += snapshot.val().num1 + snapshot.val().num2 + "=" + snapshot.val().result;
+    html += ` ${sender}: ${n1} ${operate} ${n2} = ${res} `;
     html += "</p>";
 
-    document.getElementsByClassName("show").innerHTML += html
+    
+
+   
+    show.innerHTML += html
 
   })
-
